9ba0ccb251d2e4aa03edbb31fe4c9c9f9e0bc4b1
saitejabadugu <bsaiteja10@gmail.com>
2023-04-13 23:44:59 +0530

    Handled navigation controller

--- ExpenseInfo/SceneDelegate.swift
+++ ExpenseInfo/SceneDelegate.swift
@@ -20,7 +20,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        let navVC = UINavigationController()
+        let vc = ExpenseListVC()
+        navVC.viewControllers = [vc]
+        window?.rootViewController = navVC
         window?.makeKeyAndVisible()
     }
 
